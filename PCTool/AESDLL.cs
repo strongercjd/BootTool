b2cfc79cb86de3a2c9d4706ed8f077385bb09205
Firefly <chenjindou_1993@163.com>
2018-06-13 17:43:01 +0800

    增加AESDLL的动态库项目，增加PCTool项目，且AES动态库项目编译的动态库直接放到PCTool目标文件夹内

---
+++ PCTool/AESDLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace PCTool
+{
+    class AESDLL
+    {
+        /// <summary>
+        /// AES加密初始化
+        /// </summary>
+        [DllImport("AESDLL.dll")]
+        public static extern void AesEncInit();
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="buffer"></param>
+        [DllImport("AESDLL.dll")]
+        public static extern void AesEncrypt(ref byte buffer);
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="buffer"></param>
+        [DllImport("AESDLL.dll")]
+        public static extern void AesDecrypt(ref byte buffer);
+        /// <summary>
+        /// 设置解密Key
+        /// </summary>
+        /// <param name="KeyBuffer"></param>
+        [DllImport("AESDLL.dll")]
+        public static extern void SetKey(ref byte KeyBuffer);
+    }
+}
---
+++ PCTool/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PCTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            string str = string.Empty;
+            AESDLL.AesEncInit();
+            byte[] bufferIn = new byte[16] {0X00, 0X11, 0X22, 0X33, 0X44, 0X55, 0X66, 0X77, 0X88, 0X99, 0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0XFF };
+            textBox1.Text = "明文是：";
+            textBox1.Text += "\r\n";
+            for (num = 0; num < bufferIn.Length; num++)
+            {
+                str = Convert.ToString(bufferIn[num], 16);
+                textBox1.Text += "0X" + str + " ";
+            }
+
+            AESDLL.AesEncrypt(ref bufferIn[0]);
+
+            textBox1.Text += "\r\n";
+            textBox1.Text += "\r\n";
+            textBox1.Text += "加密后密文是：";
+            textBox1.Text += "\r\n";
+            for (num = 0;num< bufferIn.Length; num++)
+            {
+                str = Convert.ToString(bufferIn[num], 16);
+                textBox1.Text += "0X"+str + " ";
+            }
+
+            AESDLL.AesDecrypt(ref bufferIn[0]);
+
+            textBox1.Text += "\r\n";
+            textBox1.Text += "\r\n";
+            textBox1.Text += "解密后明文是：";
+            textBox1.Text += "\r\n";
+            for (num = 0; num < bufferIn.Length; num++)
+            {
+                str = Convert.ToString(bufferIn[num], 16);
+                textBox1.Text += "0X" + str + " ";
+            }
+
+        }
+    }
+}
