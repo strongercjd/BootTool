19df1f34dc872ad46737227524bb2845779b1a98
Firefly <chenjindou_1993@163.com>
2018-06-15 09:21:14 +0800

    完成固件加密功能

--- PCTool/Form1.Designer.cs
+++ PCTool/Form1.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.bt_startmake = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
@@ -38,24 +38,14 @@
             this.tB_vision = new System.Windows.Forms.TextBox();
             this.cB_PCBvision = new System.Windows.Forms.ComboBox();
             this.cB_type = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.bt_out = new System.Windows.Forms.Button();
             this.bt_open = new System.Windows.Forms.Button();
-            this.tB_outpath = new System.Windows.Forms.TextBox();
             this.tB_binpath = new System.Windows.Forms.TextBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(26, 448);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(281, 151);
-            this.textBox1.TabIndex = 1;
             // 
             // groupBox1
             // 
@@ -66,24 +56,21 @@
             this.groupBox1.Controls.Add(this.tB_vision);
             this.groupBox1.Controls.Add(this.cB_PCBvision);
             this.groupBox1.Controls.Add(this.cB_type);
-            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.bt_out);
             this.groupBox1.Controls.Add(this.bt_open);
-            this.groupBox1.Controls.Add(this.tB_outpath);
             this.groupBox1.Controls.Add(this.tB_binpath);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(295, 224);
+            this.groupBox1.Size = new System.Drawing.Size(307, 182);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "生成REL";
             // 
             // bt_startmake
             // 
-            this.bt_startmake.Location = new System.Drawing.Point(16, 177);
+            this.bt_startmake.Location = new System.Drawing.Point(14, 130);
             this.bt_startmake.Name = "bt_startmake";
-            this.bt_startmake.Size = new System.Drawing.Size(257, 34);
+            this.bt_startmake.Size = new System.Drawing.Size(281, 34);
             this.bt_startmake.TabIndex = 12;
             this.bt_startmake.Text = "开始生成";
             this.bt_startmake.UseVisualStyleBackColor = true;
@@ -92,7 +79,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(196, 124);
+            this.label5.Location = new System.Drawing.Point(216, 78);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 11;
@@ -101,7 +88,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(95, 124);
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(100, 78);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 10;
@@ -110,7 +99,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 127);
+            this.label3.Location = new System.Drawing.Point(14, 78);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 9;
@@ -118,36 +107,31 @@
             // 
             // tB_vision
             // 
-            this.tB_vision.Location = new System.Drawing.Point(95, 142);
+            this.tB_vision.BackColor = System.Drawing.Color.White;
+            this.tB_vision.ForeColor = System.Drawing.Color.Red;
+            this.tB_vision.Location = new System.Drawing.Point(102, 93);
             this.tB_vision.Name = "tB_vision";
             this.tB_vision.ReadOnly = true;
-            this.tB_vision.Size = new System.Drawing.Size(71, 21);
+            this.tB_vision.Size = new System.Drawing.Size(99, 21);
             this.tB_vision.TabIndex = 8;
             // 
             // cB_PCBvision
             // 
             this.cB_PCBvision.FormattingEnabled = true;
-            this.cB_PCBvision.Location = new System.Drawing.Point(196, 142);
+            this.cB_PCBvision.Location = new System.Drawing.Point(218, 93);
             this.cB_PCBvision.Name = "cB_PCBvision";
-            this.cB_PCBvision.Size = new System.Drawing.Size(58, 20);
+            this.cB_PCBvision.Size = new System.Drawing.Size(77, 20);
             this.cB_PCBvision.TabIndex = 7;
+            this.cB_PCBvision.SelectedIndexChanged += new System.EventHandler(this.cB_PCBvision_SelectedIndexChanged);
             // 
             // cB_type
             // 
             this.cB_type.FormattingEnabled = true;
-            this.cB_type.Location = new System.Drawing.Point(14, 142);
+            this.cB_type.Location = new System.Drawing.Point(14, 93);
             this.cB_type.Name = "cB_type";
             this.cB_type.Size = new System.Drawing.Size(64, 20);
             this.cB_type.TabIndex = 6;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 12);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "REL文件";
+            this.cB_type.SelectedIndexChanged += new System.EventHandler(this.cB_type_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -158,19 +142,9 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "bin文件";
             // 
-            // bt_out
-            // 
-            this.bt_out.Location = new System.Drawing.Point(220, 87);
-            this.bt_out.Name = "bt_out";
-            this.bt_out.Size = new System.Drawing.Size(53, 23);
-            this.bt_out.TabIndex = 3;
-            this.bt_out.Text = "输出";
-            this.bt_out.UseVisualStyleBackColor = true;
-            this.bt_out.Click += new System.EventHandler(this.bt_out_Click);
-            // 
             // bt_open
             // 
-            this.bt_open.Location = new System.Drawing.Point(220, 37);
+            this.bt_open.Location = new System.Drawing.Point(242, 37);
             this.bt_open.Name = "bt_open";
             this.bt_open.Size = new System.Drawing.Size(53, 23);
             this.bt_open.TabIndex = 2;
@@ -178,32 +152,33 @@
             this.bt_open.UseVisualStyleBackColor = true;
             this.bt_open.Click += new System.EventHandler(this.bt_open_Click);
             // 
-            // tB_outpath
-            // 
-            this.tB_outpath.Location = new System.Drawing.Point(14, 89);
-            this.tB_outpath.Name = "tB_outpath";
-            this.tB_outpath.ReadOnly = true;
-            this.tB_outpath.Size = new System.Drawing.Size(197, 21);
-            this.tB_outpath.TabIndex = 1;
-            // 
             // tB_binpath
             // 
+            this.tB_binpath.BackColor = System.Drawing.Color.White;
+            this.tB_binpath.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tB_binpath.ForeColor = System.Drawing.Color.Red;
             this.tB_binpath.Location = new System.Drawing.Point(14, 39);
             this.tB_binpath.Name = "tB_binpath";
             this.tB_binpath.ReadOnly = true;
-            this.tB_binpath.Size = new System.Drawing.Size(197, 21);
+            this.tB_binpath.Size = new System.Drawing.Size(222, 21);
             this.tB_binpath.TabIndex = 0;
             this.tB_binpath.Text = "打开bin文件";
             this.tB_binpath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // openFileDialog
             // 
-            this.openFileDialog.FileName = "openFileDialog1";
-            this.openFileDialog.Filter = "仰邦bin文件(*.bin)|*.bin";
+            this.openFileDialog.Filter = "bin文件(*.bin)|*.bin";
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.Title = "打开bin文件";
             // 
             // saveFileDialog
             // 
             this.saveFileDialog.Filter = "升级固件(*.REL)|*.REL";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
@@ -211,19 +186,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 611);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "生产工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button bt_startmake;
         private System.Windows.Forms.Label label5;
@@ -232,14 +205,12 @@
         private System.Windows.Forms.TextBox tB_vision;
         private System.Windows.Forms.ComboBox cB_PCBvision;
         private System.Windows.Forms.ComboBox cB_type;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button bt_out;
         private System.Windows.Forms.Button bt_open;
-        private System.Windows.Forms.TextBox tB_outpath;
         private System.Windows.Forms.TextBox tB_binpath;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- PCTool/Form1.cs
+++ PCTool/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Drawing.Imaging;
 
 namespace PCTool
 {
@@ -16,60 +19,32 @@ namespace PCTool
     {
         String BinPath = "";
         String RELPath = "";
+
+        byte deicve_type;
+        byte PCBversion;
+        
+
+        //24字节                    /*上海仰邦科技股份有限公司*/
+        byte[] company = new byte[] { 0XC9, 0XCF, 0XBA, 0XA3, 0XD1, 0XF6, 0XB0, 0XEE, 0XBF, 0XC6, 0XBC, 0XBC, 0XB9, 0XC9, 0XB7, 0XDD, 0XD3, 0XD0, 0XCF, 0XDE, 0XB9, 0XAB, 0XCB, 0XBE
+                                      /*onbonbx.com*/
+                                     ,0x6F,0x6E,0x62,0x6F,0x6E,0x62,0x78,0x2E,0x63,0x6F,0x6D};
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            int num;
-            string str = string.Empty;
-            AESDLL.AesEncInit();
-            byte[] bufferIn = new byte[16] {0X00, 0X11, 0X22, 0X33, 0X44, 0X55, 0X66, 0X77, 0X88, 0X99, 0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0XFF };
-            textBox1.Text = "明文是：";
-            textBox1.Text += "\r\n";
-            for (num = 0; num < bufferIn.Length; num++)
-            {
-                str = Convert.ToString(bufferIn[num], 16);
-                textBox1.Text += "0X" + str + " ";
-            }
-
-            AESDLL.AesEncrypt(ref bufferIn[0]);
-
-            textBox1.Text += "\r\n";
-            textBox1.Text += "\r\n";
-            textBox1.Text += "加密后密文是：";
-            textBox1.Text += "\r\n";
-            for (num = 0;num< bufferIn.Length; num++)
-            {
-                str = Convert.ToString(bufferIn[num], 16);
-                textBox1.Text += "0X"+str + " ";
-            }
-
-            AESDLL.AesDecrypt(ref bufferIn[0]);
-
-            textBox1.Text += "\r\n";
-            textBox1.Text += "\r\n";
-            textBox1.Text += "解密后明文是：";
-            textBox1.Text += "\r\n";
-            for (num = 0; num < bufferIn.Length; num++)
-            {
-                str = Convert.ToString(bufferIn[num], 16);
-                textBox1.Text += "0X" + str + " ";
-            }
-
         }
 
         private void bt_open_Click(object sender, EventArgs e)
         {
             try
             {
-                OpenFileDialog dialog = new OpenFileDialog();
-                if (dialog.ShowDialog() == DialogResult.OK)
+                //OpenFileDialog dialog = new OpenFileDialog();
+                //openFileDialog.FileName = "1";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    BinPath = dialog.FileName;
-                    tB_binpath.Text = dialog.SafeFileName;
+                    BinPath = openFileDialog.FileName;
+                    tB_binpath.ForeColor = Color.Black;
+                    tB_binpath.Text = openFileDialog.SafeFileName;
                 }
             }
             catch (Exception ex)
@@ -78,43 +53,47 @@ namespace PCTool
             }
         }
 
-        private void bt_out_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-
-                    RELPath = saveFileDialog.FileName;
-                    tB_outpath.Text = RELPath;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-        }
 
         private void bt_startmake_Click(object sender, EventArgs e)
         {
             Byte[] data = new byte[1024*1024];
             Byte[] bufferEnc = new byte[1024*1024];
             Int32 file_len;
+            Int32 save_file_len;
+
+            if (BinPath == "")//如果路径是空的
+            {
+                MessageBox.Show("请选择需要转换的目标文件!         ", "错误");
+                return;
+            }
+
             try
             {
-                if (BinPath == "")//如果路径是空的
+                this.saveFileDialog.FileName = cB_type.Text+"-"+ tB_vision.Text+ "-" + cB_PCBvision.Text;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    MessageBox.Show("请选择需要转换的目标文件!         ", "错误");
-                    return;
+                    RELPath = saveFileDialog.FileName;
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
 
 
+            try
+            {
                 FileStream fs_read = new FileStream(BinPath, FileMode.Open, FileAccess.Read);
                 BinaryReader br = new BinaryReader(fs_read);
                 file_len = (int)fs_read.Length;//获取bin文件长度
 
-                if (file_len < (1024*1024))
+                if (file_len < (1024 * 1024))
                 {
+                    bt_open.Enabled = false;
+                    cB_type.Enabled = false;
+                    tB_vision.Enabled = false;
+                    cB_PCBvision.Enabled = false;
+
                     br.BaseStream.Seek(0, SeekOrigin.Begin);
                     for (int num = 0; num < file_len; num++)
                     {
@@ -127,11 +106,28 @@ namespace PCTool
                     uint i;
                     uint j = 0;
 
-
                     //// 创建加密缓存及加密处理
                     byte[] buffer = new byte[16];
 
+
                     AESDLL.AesEncInit();
+
+                    //128个字节的公司信息
+                    for (j = 0; j < company.Length; j++)
+                    {
+                        bufferEnc[j] = company[j];
+                    }
+                    //32个字节版本信息，1字节型号，8个字节主型号，1个字节PCB版本
+                    byte[] vision = System.Text.Encoding.Default.GetBytes(tB_vision.Text);
+                    bufferEnc[128] = deicve_type;
+                    for (j = 0; j < vision.Length; j++)
+                    {
+                        bufferEnc[129+j] = vision[j];
+                    }
+                    bufferEnc[129+ vision.Length] = PCBversion;
+
+
+
                     for (i = 0; i < (file_len / 16 + 1); i++)
                     {
                         for (j = 0; j < 16; j++)
@@ -145,7 +141,7 @@ namespace PCTool
                         AESDLL.AesDecrypt(ref buffer[0]);
                         for (j = 0; j < 16; j++)
                         {
-                            bufferEnc[i * 16 + j] = buffer[j];
+                            bufferEnc[i * 16 + j+128+32] = buffer[j];
                         }
                     }
 
@@ -153,21 +149,29 @@ namespace PCTool
                     if (RELPath == "")
                     {
                         RELPath = Path.ChangeExtension(BinPath, "Bin");
-                        tB_outpath.Text = RELPath;
                     }
                     FileStream fBin = new FileStream(RELPath, FileMode.Create); //创建文件BIN文件
                     BinaryWriter BinWrite = new BinaryWriter(fBin); //二进制方式打开文件
                                                                     // 数据写入文件
 
-                    BinWrite.Write(bufferEnc, 0, bufferEnc.Length); //写入数据
-                    //BinWrite.Write(bufferEnc, 0, file_len); //写入数据
+                    //BinWrite.Write(bufferEnc, 0, bufferEnc.Length); //写入数据
+                    save_file_len = 128 + 32 + (file_len / 16 + 1) * 16;
+                    BinWrite.Write(bufferEnc, 0, save_file_len); //写入数据
 
                     BinWrite.Flush();//释放缓存
                     BinWrite.Close();//关闭文件
 
+
+                    bt_open.Enabled = true;
+                    cB_type.Enabled = true;
+                    tB_vision.Enabled = true;
+                    cB_PCBvision.Enabled = true;
+
+
                     MessageBox.Show("文件转换完成!        ", "提示");
                 }
-                else {
+                else
+                {
                     MessageBox.Show("文件大于1M!        ", "错误");
                 }
             }
@@ -176,5 +180,177 @@ namespace PCTool
                 MessageBox.Show(ex.ToString());
             }
         }
+
+        string xmlPath = System.Environment.CurrentDirectory + "\\config.xml";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                AESDLL.AesEncInit();
+            }
+            catch
+            {
+                MessageBox.Show("缺少AESDLL.dll文件!        ", "提示");
+                Application.Exit();
+            }
+            if (File.Exists(xmlPath))
+            {
+                XmlDocument doc = new XmlDocument();
+                //加载根目录下XML文件
+                doc.Load(xmlPath);
+                //获取根节点
+                XmlElement root = doc.DocumentElement;
+                //获取<controller>子节点 
+                XmlNodeList controllerNodes = root.GetElementsByTagName("controller");
+
+                //使用foreach循环读出集合
+                foreach (XmlNode node in controllerNodes)
+                {
+                    ////string controller_name = ((XmlElement)node).GetElementsByTagName("controller_name")[0].InnerText;
+                    string controller_name = ((XmlElement)node).GetAttribute("name");
+                    cB_type.Items.Add(controller_name);
+                }
+                cB_type.SelectedIndex = 0;
+                doc.Save(xmlPath);
+
+                //tSt = str.Substring(str.Length - i);
+                string strtime = DateTime.Now.Year.ToString();
+                tB_vision.Text = strtime.Substring(strtime.Length - 2);
+
+                if (DateTime.Now.Month < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Month.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Month.ToString();
+
+                if (DateTime.Now.Hour < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Hour.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Hour.ToString();
+                if (DateTime.Now.Minute < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Minute.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Minute.ToString();
+            }
+            else
+            {
+                MessageBox.Show("没有配置文件        ", "错误");
+            }
+        }
+
+        private void cB_type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cB_PCBvision.Items.Clear();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+            ////获取根节点
+            XmlElement root = doc.DocumentElement;
+            ////获取root下<controller>子节点 
+            //XmlNodeList controllerlist = root.GetElementsByTagName("controller");
+            ///获取root下所有子节点
+            XmlNodeList rootlist = root.ChildNodes;
+            if (rootlist != null)
+            {
+                foreach (XmlNode controllerNode in rootlist)
+                {
+                    if (controllerNode.Name == "controller")
+                    {
+                        string controller_name = ((XmlElement)controllerNode).GetAttribute("name");
+                        if (controller_name == cB_type.Text)
+                        {
+                            string controller_type_value = ((XmlElement)controllerNode).GetElementsByTagName("type_value")[0].InnerText;
+                            controller_type_value = controller_type_value.Remove(0, 2);//去掉前两个字符
+                            deicve_type = Convert.ToByte(controller_type_value, 16);
+                          
+                            XmlNodeList PCBvisionList = controllerNode.ChildNodes;
+                            if (PCBvisionList != null)
+                            {
+                                foreach (XmlNode PCBvisionnode in PCBvisionList)
+                                {
+                                    if (PCBvisionnode.Name == "PCBvision")
+                                    {
+                                        string PCBvision_name = ((XmlElement)PCBvisionnode).GetAttribute("name");
+                                        cB_PCBvision.Items.Add(PCBvision_name);
+                                    }
+                                }
+                                cB_PCBvision.SelectedIndex = 0;
+                            }
+                        }
+                    }  
+                }
+            }
+        }
+
+        private void cB_PCBvision_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int num;
+            num = 0;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+            ////获取根节点
+            XmlElement root = doc.DocumentElement;
+            ////获取root下<controller>子节点 
+            //XmlNodeList controllerlist = root.GetElementsByTagName("controller");
+            ///获取root下所有子节点
+            XmlNodeList rootlist = root.ChildNodes;
+            if (rootlist != null)
+            {
+                foreach (XmlNode controllerNode in rootlist)
+                {
+                    if (controllerNode.Name == "controller")
+                    {
+                        string controller_name = ((XmlElement)controllerNode).GetAttribute("name");
+                        if (controller_name == cB_type.Text)
+                        {
+                            XmlNodeList PCBvisionList = controllerNode.ChildNodes;
+                            if (PCBvisionList != null)
+                            {
+                                foreach (XmlNode PCBvisionnode in PCBvisionList)
+                                {
+                                    if (PCBvisionnode.Name == "PCBvision")
+                                    {
+                                        num++;
+                                        string PCBvision_name = ((XmlElement)PCBvisionnode).GetAttribute("name");
+                                        if (PCBvision_name == cB_PCBvision.Text)
+                                        {
+                                            num--;
+                                            string PCBvision_value = ((XmlElement)controllerNode).GetElementsByTagName("PCBvision")[num].InnerText;
+                                            PCBvision_value = PCBvision_value.Remove(0, 2);//去掉前两个字符
+                                            PCBversion = Convert.ToByte(PCBvision_value, 16);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        string timenum;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((DateTime.Now.Minute.ToString()) != timenum)
+            {
+                timenum = DateTime.Now.Minute.ToString();
+
+                string strtime = DateTime.Now.Year.ToString();
+                tB_vision.Text = strtime.Substring(strtime.Length - 2);
+
+                if (DateTime.Now.Month < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Month.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Month.ToString();
+
+                if (DateTime.Now.Hour < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Hour.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Hour.ToString();
+                if (DateTime.Now.Minute < 10)
+                    tB_vision.Text += "0" + DateTime.Now.Minute.ToString();
+                else
+                    tB_vision.Text += DateTime.Now.Minute.ToString();
+            }
+        }
     }
 }
+
