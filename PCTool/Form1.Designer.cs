6dcf678e9cf4a40362b54d7563243166a7ad9642
strongercjd <strongercjd@163.com>
2019-10-11 16:19:07 +0800

    编译器升级为VS2017

--- PCTool/Form1.Designer.cs
+++ PCTool/Form1.Designer.cs
@@ -184,7 +184,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 611);
+            this.ClientSize = new System.Drawing.Size(331, 202);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
